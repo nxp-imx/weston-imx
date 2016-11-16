ea4e7fd200179f2f616e0956f20fe0e5a24fadd6
Daniel Stone <daniels@collabora.com>
2016-11-16 11:51:27 +0000

    compositor-drm: Add to_drm_mode helper
    Much like we already have to_drm_output and to_drm_backend.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Reviewed-by: Pekka Paalanen <pekka.paalanen@collabora.co.uk>

--- libweston/compositor-drm.c
+++ libweston/compositor-drm.c
@@ -496,6 +496,12 @@ drm_output_pageflip_timer_create(struct drm_output *output)
 	return 0;
 }
 
+static inline struct drm_mode *
+to_drm_mode(struct weston_mode *base)
+{
+	return container_of(base, struct drm_mode, base);
+}
+
 /**
  * Get the current value of a KMS property
  *
@@ -1868,7 +1874,7 @@ drm_output_apply_state(struct drm_output_state *state)
 	assert(scanout_state->dest_w == scanout_state->src_w >> 16);
 	assert(scanout_state->dest_h == scanout_state->src_h >> 16);
 
-	mode = container_of(output->base.current_mode, struct drm_mode, base);
+	mode = to_drm_mode(output->base.current_mode);
 	if (backend->state_invalid || !scanout_plane->state_cur->fb ||
 	    scanout_plane->state_cur->fb->stride != scanout_state->fb->stride) {
 		ret = drmModeSetCrtc(backend->drm.fd, output->crtc_id,
@@ -2775,7 +2781,7 @@ choose_mode (struct drm_output *output, struct weston_mode *target_mode)
 	    output->base.current_mode->height == target_mode->height &&
 	    (output->base.current_mode->refresh == target_mode->refresh ||
 	     target_mode->refresh == 0))
-		return (struct drm_mode *)output->base.current_mode;
+		return to_drm_mode(output->base.current_mode);
 
 	wl_list_for_each(mode, &output->base.mode_list, base.link) {
 		if (mode->mode_info.hdisplay == target_mode->width &&
