3b08f62d5a92837e23e246f90dfb089dc71cc5f8
Yong Gan <yong.gan@nxp.com>
2017-12-05 08:34:03 +0800

    compositor: vdk performace limit to 30 fps with 4k and 60fps display setting.
    Change the DEFAULT_REPAINT_WINDOW from 7ms to 16ms.
    It need more time to repaint window in 4k mode.
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Yong Gan <yong.gan@nxp.com>

--- libweston/compositor.c
+++ libweston/compositor.c
@@ -64,7 +64,7 @@
 #include "version.h"
 #include "plugin-registry.h"
 
-#define DEFAULT_REPAINT_WINDOW 7 /* milliseconds */
+#define DEFAULT_REPAINT_WINDOW 16 /* milliseconds */
 
 static void
 weston_output_transform_scale_init(struct weston_output *output,
