75e74639cfdfee0fef56c0857ab80f7962a2d13f
Haihua Hu <jared.hu@nxp.com>
2018-08-29 12:49:08 +0800

    compositor-drm: support get drm fb from dmabuf
    directly use gem object to improt dmabuf, cache gem
    handle in dmabuf object to avoid re-import dmabuf fd every time
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>
    (cherry picked from commit 60e5f7422f07837b6cfc74908174f6c0c945e74e)
    (cherry picked from commit 3b9fbdfa8325f84a58868c69594f1fd583fe434b)

--- libweston/backend-drm/drm-internal.h
+++ libweston/backend-drm/drm-internal.h
@@ -82,6 +82,8 @@
 #define DRM_PLANE_ALPHA_OPAQUE	0xffffUL
 #endif
 
+#define ALIGNTO(a, b) ((a + (b-1)) & (~(b-1)))
+
 /**
  * A small wrapper to print information into the 'drm-backend' debug scope.
  *
