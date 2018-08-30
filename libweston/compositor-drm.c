4bb5b8ecd212614727e63bb975ec40da77120913
Haihua Hu <jared.hu@nxp.com>
2018-08-30 13:03:08 +0800

    compositor-drm: fix primary fb format is not argb format
    when we need to use argb format on 850D, don't change it to
    xrgb, this will make video unvisuable. Other platform, will
    default use xrgb format.
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>

--- libweston/compositor-drm.c
+++ libweston/compositor-drm.c
@@ -1280,11 +1280,6 @@ drm_fb_get_from_bo(struct gbm_bo *bo, struct drm_backend *backend,
 		goto err_free;
 	}
 
-	/* We can scanout an ARGB buffer if the surface's opaque region covers
-	 * the whole output, but we have to use XRGB as the KMS format code. */
-	if (is_opaque)
-		fb->format = pixel_format_get_opaque_substitute(fb->format);
-
 	if (backend->min_width > fb->width ||
 	    fb->width > backend->max_width ||
 	    backend->min_height > fb->height ||
