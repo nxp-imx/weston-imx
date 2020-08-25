ad338a66b5b2d882461dff913c3460819e0faf8f
Yong Gan <yong.gan@nxp.com>
2020-08-25 16:30:18 +0800

    compositor: Add G2d render in DRM compositor
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Yong Gan <yong.gan@nxp.com>
    (cherry picked from commit 89d78111d41316c22adb781f8b5f1fb9a9e5c15b)

--- include/libweston/backend-drm.h
+++ include/libweston/backend-drm.h
@@ -193,6 +193,11 @@ struct weston_drm_backend_config {
 	/** Whether to use the pixman renderer instead of the OpenGL ES renderer. */
 	bool use_pixman;
 
+#if defined(ENABLE_IMXG2D)
+	/** Whether to use the g2d renderer instead of the OpenGL ES renderer. */
+	bool use_g2d;
+#endif
+
 	/** The seat to be used for input and output.
 	 *
 	 * If seat_id is NULL, the seat is taken from XDG_SEAT environment
@@ -207,6 +212,7 @@ struct weston_drm_backend_config {
 	 * Valid values are:
 	 * - NULL - The default format ("xrgb8888") will be used;
 	 * - "xrgb8888";
+	 * - "argb8888"
 	 * - "rgb565"
 	 * - "xrgb2101010"
 	 * The backend will take ownership of the format pointer and will free
