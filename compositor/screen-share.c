1123cf4a303924c40c9282565a881b90688d1a9f
Wujian sun <wujian.sun_1@nxp.com>
2021-10-26 15:46:00 +0800

    screen-share: screen-share fail to work on rdp-backend with g2d-renderer
    G2d-renderer do y-flip when blit in g2d_renderer_read_pixels,
    so no need in shared_output_repainted.
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Wujian sun <wujian.sun_1@nxp.com>
    (cherry picked from commit f8ce046f001f670100959b0aa9e2ffd2f8fc820a)
    (cherry picked from commit e3b9c7beee1461e083174644ed6e936f726cc58c)
    (cherry picked from commit 7418b458e0600b1698b62abb80df07fc8d33db5b)

--- compositor/screen-share.c
+++ compositor/screen-share.c
@@ -821,6 +821,8 @@ shared_output_repainted(struct wl_listener *listener, void *data)
 {
 	struct shared_output *so =
 		container_of(listener, struct shared_output, frame_listener);
+	struct weston_config *config;
+	struct weston_config_section *section;
 	pixman_region32_t damage;
 	pixman_region32_t *current_damage = data;
 	struct ss_shm_buffer *sb;
@@ -832,6 +834,13 @@ shared_output_repainted(struct wl_listener *listener, void *data)
 	const struct pixel_format_info *read_format =
 		so->output->compositor->read_format;
 	const pixman_format_code_t pixman_format = read_format->pixman_format;
+
+	uint32_t use_g2d;
+
+	config = wet_get_config(so->output->compositor);
+	section = weston_config_get_section(config, "core", NULL, NULL);
+
+	weston_config_section_get_uint(section, "use-g2d", &use_g2d, 0);
 
 	width = so->output->current_mode->width;
 	height = so->output->current_mode->height;
@@ -880,7 +889,7 @@ shared_output_repainted(struct wl_listener *listener, void *data)
 		width = r[i].x2 - r[i].x1;
 		height = r[i].y2 - r[i].y1;
 
-		if (do_yflip)
+		if (do_yflip && !use_g2d)
 			y_orig = so->output->current_mode->height - r[i].y2;
 		else
 			y_orig = y;
