41e8291c4a9d010ed731f1f9aac5f8aa726656b2
Xianzhong <xianzhong.li@nxp.com>
2021-11-26 13:14:13 +0800

    g2d-renderer/drm: add get_surface_fence_fd in g2d_renderer
    drm/kms invoke the function to get a fence fd for drm-in fence
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>
    (cherry picked from commit bb7f487baa2fd21c1b2e7414bff5ca2bc1a81dea)
    (cherry picked from commit 4e6943d6d0daa30a4d4dc19f7c515c6caef931cb)
    (cherry picked from commit 6de23317430ce14560454d22211d7e00155e10ca)

--- libweston/renderer-g2d/g2d-renderer.c
+++ libweston/renderer-g2d/g2d-renderer.c
@@ -2013,6 +2013,12 @@ g2d_renderer_output_set_buffer(struct weston_output *output, struct g2d_surfaceE
 }
 
 static int
+g2d_renderer_get_surface_fence_fd(struct g2d_surfaceEx *buffer)
+{
+	return buffer->reserved[0];
+}
+
+static int
 g2d_drm_renderer_output_create(struct weston_output *output)
 {
 	struct g2d_output_state *go;
@@ -2059,6 +2065,7 @@ drm_create_g2d_image(struct g2d_surfaceEx* g2dSurface,
 	g2dSurface->base.rot	= G2D_ROTATION_0;
 	g2dSurface->base.clrcolor = 0xFF400000;
 	g2dSurface->tiling = G2D_LINEAR;
+	g2dSurface->reserved[0] = -1;
 
 	return 0;
 }
@@ -2333,4 +2340,5 @@ g2d_fbdev_renderer_output_create(struct weston_output *output,
 	.create_g2d_image    = drm_create_g2d_image,
 	.output_set_buffer   = g2d_renderer_output_set_buffer,
 	.output_destroy      = g2d_renderer_output_destroy,
+	.get_surface_fence_fd = g2d_renderer_get_surface_fence_fd,
 };
