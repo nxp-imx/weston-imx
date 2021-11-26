f503e6a5e94d49bf4742433b2ca38116dc5a3218
Xianzhong <xianzhong.li@nxp.com>
2021-11-26 21:09:34 +0800

    g2d-renderer: increase drmfb dumb buffer with 3
    g2d renderer has screen flicker with 2 dumb buffers,
      GLES2.Bloom_Wayland --FinalBloom 0
    
    gl-renderer has no problem with 3 gbm buffers.
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>
    (cherry picked from commit 82c63feb327f62f2d98fd6053ef498dd6b0cdea9)
    (cherry picked from commit fa2a1d803416285a431bdc47a53504d6d503e241)

--- libweston/backend-drm/drm-internal.h
+++ libweston/backend-drm/drm-internal.h
@@ -631,11 +631,11 @@ struct drm_output {
 	/* only set when a writeback screenshot is ongoing */
 	struct drm_writeback_state *wb_state;
 
-	struct drm_fb *dumb[2];
-	weston_renderbuffer_t renderbuffer[2];
+	struct drm_fb *dumb[3];
+	weston_renderbuffer_t renderbuffer[3];
 #if defined(ENABLE_IMXG2D)
-	struct g2d_surfaceEx g2d_image[2];
-	int dumb_dmafd[2];
+	struct g2d_surfaceEx g2d_image[3];
+	int dumb_dmafd[3];
 #endif
 	int current_image;
 	pixman_region32_t previous_damage;
