636c70464340c621cae1f9d5b04056c437c8b28f
Wujian Sun <wujian.sun_1@nxp.com>
2022-07-21 13:31:49 +0800

    backend-drm: fail to update hdcp status
    Need add DRM_MODE_ATOMIC_ALLOW_MODESET flag to
    update hdcp status.
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Wujian Sun <wujian.sun_1@nxp.com>
    (cherry picked from commit 2dc02471ea64bb6ab649d9f4befeaaa38ab3c67a)

--- libweston/backend-drm/kms.c
+++ libweston/backend-drm/kms.c
@@ -857,6 +857,28 @@ get_drm_protection_from_weston(enum weston_hdcp_protection weston_protection,
 	}
 }
 
+static int
+drm_protection_from_weston_update(enum weston_hdcp_protection protection)
+{
+	enum weston_hdcp_protection current_protection;
+	static enum weston_hdcp_protection op_protection;
+	static bool op_protection_valid = false;
+
+	current_protection = protection;
+
+	if (!op_protection_valid) {
+		op_protection = current_protection;
+		op_protection_valid = true;
+	}
+
+	if (current_protection != op_protection) {
+		op_protection = current_protection;
+		return 1;
+	}
+
+	return 0;
+}
+
 static void
 drm_connector_set_hdcp_property(struct drm_connector *connector,
 				enum weston_hdcp_protection protection,
@@ -948,6 +970,7 @@ drm_output_apply_state_atomic(struct drm_output_state *state,
 	struct drm_head *tmp;
 	int ret = 0;
 	int in_fence_fd = -1;
+	int update = 0;
 
 	if(output->gbm_surface) {
 		/* in_fence_fd was not created when
@@ -1008,8 +1031,15 @@ drm_output_apply_state_atomic(struct drm_output_state *state,
 	}
 
 	wl_list_for_each(head, &output->base.head_list, base.output_link) {
-		drm_connector_set_hdcp_property(&head->connector,
-						state->protection, req);
+		update = drm_protection_from_weston_update(state->protection);
+		if(update) {
+			drm_connector_set_hdcp_property(&head->connector,
+							state->protection, req);
+			/* checking if the output driver this head */
+			if (head->base.output == &output->base) {
+				*flags |= DRM_MODE_ATOMIC_ALLOW_MODESET;
+			}
+		}
 
 		if (drm_connector_has_prop(&head->connector,
 					   WDRM_CONNECTOR_HDR_OUTPUT_METADATA)) {
