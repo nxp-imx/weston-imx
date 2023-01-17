cf16334b614fcce351f5e82a3f9e74c938684c16
Wujian Sun <wujian.sun_1@nxp.com>
2023-01-17 11:10:19 +0800

    backend-drm: dmabuf leak during hdmi hotplug
    Close dumb buffer dmafd when output destoryed.
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Wujian Sun <wujian.sun_1@nxp.com>
    (cherry picked from commit d7863145cddd13a30deabf978c40bb5521859ef2)

--- libweston/backend-drm/drm-internal.h
+++ libweston/backend-drm/drm-internal.h
@@ -635,6 +635,7 @@ struct drm_output {
 	weston_renderbuffer_t renderbuffer[2];
 #if defined(ENABLE_IMXG2D)
 	struct g2d_surfaceEx g2d_image[2];
+	int dumb_dmafd[2];
 #endif
 	int current_image;
 	pixman_region32_t previous_damage;
