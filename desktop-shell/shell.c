83246b16f01374256bb1a3b4085e57853fac591a
Erkai Ji <erkai.ji@nxp.com>
2023-11-21 03:11:18 +0800

    desktop-shell: Fix weston crash when running MM test case
    below patch will check if power state is in the normal state.
    DPMS has four states in all, but only two states are listed in
    weston_output_power_state, this will cause weston crash if
    hot-plugging when running MM case, which is in suspend state.
    
    Revert "desktop-shell: Fade animation should base on output power state"
    
    This reverts commit a1b7f1a5a25bc21417181a0ef4526120d679b650.
    
    Signed-off-by: Erkai Ji <erkai.ji@nxp.com>
    (cherry picked from commit ca4dae936110a8ed8158ee8112148872ac6a624a)

--- desktop-shell/shell.c
+++ desktop-shell/shell.c
@@ -2017,8 +2017,7 @@ desktop_surface_removed(struct weston_desktop_surface *desktop_surface,
 	if (weston_view_is_mapped(shsurf->view) &&
 	    shsurf->shell->win_close_animation_type == ANIMATION_FADE) {
 
-		if (shsurf->shell->compositor->state == WESTON_COMPOSITOR_ACTIVE &&
-		    shsurf->view->output->power_state == WESTON_OUTPUT_POWER_NORMAL) {
+		if (shsurf->shell->compositor->state == WESTON_COMPOSITOR_ACTIVE) {
 			struct weston_coord_global pos;
 
 			pixman_region32_fini(&surface->pending.input);
