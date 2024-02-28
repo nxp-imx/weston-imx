16b0f2ca8b9679d8c8317b679822974303606586
Haihua Hu <jared.hu@nxp.com>
2024-02-28 16:11:11 +0900

    LF-11440 g2d-renderer: fix buffer alignment mismatch with upstream buffer
    No need set aligment size to g2d surface.width/height, the internal
    calculated value has chance to mismatch with upstream buffer stride
    and width > stride, this will cause blit error (eg, upstream buffer
    stride is 8 aligment, but internal aligment is 16 for width)
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>
    (cherry picked from commit 3ba1ccc7daf74e39ab922a815f7a5d489cd18751)

--- libweston/renderer-g2d/g2d-renderer.c
+++ libweston/renderer-g2d/g2d-renderer.c
@@ -1404,7 +1404,7 @@ g2d_renderer_attach_dmabuf(struct weston_surface *es, struct  weston_buffer *buf
 {
 	struct g2d_surface_state *gs = get_surface_state(es);
 	struct linux_dmabuf_buffer *dmabuf = buffer->dmabuf;
-	int alignedWidth = 0, alignedHeight = 0;
+	int alignedWidth = 0;
 	enum g2d_format g2dFormat;
 	unsigned int *paddr;
 	int i = 0;
@@ -1415,10 +1415,6 @@ g2d_renderer_attach_dmabuf(struct weston_surface *es, struct  weston_buffer *buf
 	if(dmabuf->attributes.modifier[0] == DRM_FORMAT_MOD_VIVANTE_SUPER_TILED ||
 	   dmabuf->attributes.modifier[0] == DRM_FORMAT_MOD_VIVANTE_SPLIT_SUPER_TILED) {
 		alignedWidth  = ALIGN_TO_64(buffer->width);
-		alignedHeight = ALIGN_TO_64(buffer->height);
-	} else {
-		alignedWidth  = ALIGN_TO_16(buffer->width);
-		alignedHeight = ALIGN_TO_16(buffer->height);
 	}
 	g2d_renderer_get_g2dformat_from_dmabuf(dmabuf->attributes.format, &g2dFormat, &bpp);
 
@@ -1434,8 +1430,8 @@ g2d_renderer_attach_dmabuf(struct weston_surface *es, struct  weston_buffer *buf
 	gs->g2d_surface.base.top  = 0;
 	gs->g2d_surface.base.right	= buffer->width;
 	gs->g2d_surface.base.bottom = buffer->height;
-	gs->g2d_surface.base.width	= alignedWidth;
-	gs->g2d_surface.base.height = alignedHeight;
+	gs->g2d_surface.base.width	= buffer->width;
+	gs->g2d_surface.base.height = buffer->height;
 	gs->g2d_surface.base.rot	= G2D_ROTATION_0;
 	if (dmabuf->attributes.modifier[0] == DRM_FORMAT_MOD_AMPHION_TILED) {
 		gs->g2d_surface.base.stride = dmabuf->attributes.stride[0];
