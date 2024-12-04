8c90849ba0b1cf84cf89b948d5dba346503fe789
Erkai Ji <erkai.ji@nxp.com>
2024-12-04 21:51:33 +0800

    compositor: Fix glmark performance decline issue
    The below commit limits glmark performance to 120fps, which
    is a big drop. This is a workaround that attach buffer earlier
    to improve performance.
    
    c08a6ff8bd6e1839a8fd7fe87602df9a1a8b62e2
    compositor/renderer: Only attach buffer to renderer in repaint
    
    Upstream-Status: Inappropriate [i.MX-specific]
    
    Signed-off-by: Erkai Ji <erkai.ji@nxp.com>

--- libweston/compositor.c
+++ libweston/compositor.c
@@ -3268,6 +3268,7 @@ weston_surface_attach(struct weston_surface *surface,
 {
 	struct weston_buffer *buffer = state->buffer;
 	struct weston_buffer *old_buffer = surface->buffer_ref.buffer;
+	struct weston_paint_node *pnode;
 
 	if (!buffer) {
 		if (weston_surface_is_mapped(surface)) {
@@ -3325,6 +3326,12 @@ weston_surface_attach(struct weston_surface *surface,
 	old_buffer = NULL;
 	weston_buffer_reference(&surface->buffer_ref, buffer,
 				BUFFER_MAY_BE_ACCESSED);
+
+	wl_list_for_each(pnode, &surface->paint_node_list, surface_link) {
+		assert(pnode->surface == surface);
+		surface->compositor->renderer->attach(pnode);
+		break;
+	}
 
 	return status;
 }
