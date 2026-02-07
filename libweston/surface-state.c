b4cec9be947fe6da3638173c853e756dbf5ab5d0
Elliot Chen <elliot.chen@nxp.com>
2026-02-07 11:15:01 +0900

    LF-17520 compositor: do not assert view_list_needs_rebuild when applying subsurface order
    view_list_needs_rebuild may not always be configured to true
    when applying subsurface order on 8mq board.
    
    Signed-off-by: Elliot Chen <elliot.chen@nxp.com>

--- libweston/surface-state.c
+++ libweston/surface-state.c
@@ -275,7 +275,6 @@ weston_surface_attach(struct weston_surface *surface,
 static void
 weston_surface_apply_subsurface_order(struct weston_surface *surface)
 {
-	struct weston_compositor *comp = surface->compositor;
 	struct weston_subsurface *sub;
 	struct weston_view *view;
 
@@ -286,7 +285,6 @@ weston_surface_apply_subsurface_order(struct weston_surface *surface)
 		wl_list_for_each(view, &sub->surface->views, surface_link)
 			weston_view_geometry_dirty(view);
 	}
-	weston_assert_true(comp, comp->view_list_needs_rebuild);
 }
 
 /* Translate pending damage in buffer co-ordinates to surface
