nxp-imx/weston-imx

commit be368c1b9e5a002b78bf8529174b2ae0f712fddd

compositor-drm: support get drm fb from dmabuf
directly use gem object to import dmabuf, cache gem
handle in dmabuf object to avoid re-import dmabuf fd every time

Upstream-Status: Inappropriate [i.MX-specific]

Signed-off-by: Haihua Hu <jared.hu@nxp.com>
(cherry picked from commit 60e5f7422f07837b6cfc74908174f6c0c945e74e)
(cherry picked from commit 3b9fbdfa8325f84a58868c69594f1fd583fe434b)
(cherry picked form commit 5f61ae076fbbc401bd95f4e216198a568b64aaca)

libweston/backend-drm/fb.c
@@ -402,6 +402,21 @@ drm_fb_destroy_dmabuf(struct drm_fb *fb)
 	drm_fb_destroy(fb);
 }
 
+static void
+drm_close_gem_handle(struct linux_dmabuf_buffer *dmabuf)
+{
+	struct drm_backend *b = to_drm_backend(dmabuf->compositor);
+	int i;
+
+	if (dmabuf->gem_handles[0] != 0) {
+		for (i = 0; i < dmabuf->attributes.n_planes; i++) {
+			struct drm_gem_close arg = { dmabuf->gem_handles[i], };
+			drmIoctl (b->drm->drm.fd, DRM_IOCTL_GEM_CLOSE, &arg);
+			dmabuf->gem_handles[i] = 0;
+		}
+	}
+}
+
 struct drm_fb *
 drm_fb_get_from_dmabuf_attributes(struct dmabuf_attributes *attributes,
 				  struct drm_device *device, bool is_opaque,
@@ -411,6 +426,8 @@ drm_fb_get_from_dmabuf_attributes(struct dmabuf_attributes *attributes,
 	struct drm_backend *backend = device->backend;
 	struct drm_fb *fb;
 	int i;
+	uint32_t gem_handle[MAX_DMABUF_PLANES] = {0};
+	struct linux_dmabuf_buffer *dmabuf = container_of(attributes, struct linux_dmabuf_buffer, attributes);
 	struct gbm_import_fd_modifier_data import_mod = {
 		.width = attributes->width,
 		.height = attributes->height,
@@ -453,10 +470,6 @@ drm_fb_get_from_dmabuf_attributes(struct dmabuf_attributes *attributes,
 	fb->type = is_internal ? BUFFER_DMABUF_BACKEND : BUFFER_DMABUF;
 	fb->backend = device->backend;
 
-	ARRAY_COPY(import_mod.fds, attributes->fd);
-	ARRAY_COPY(import_mod.strides, attributes->stride);
-	ARRAY_COPY(import_mod.offsets, attributes->offset);
-
 	/* skip bo import if dmabuf is using direct-display extension */
 	if (direct_display) {
 		fb->direct_display = true;
@@ -473,15 +486,6 @@ drm_fb_get_from_dmabuf_attributes(struct dmabuf_attributes *attributes,
 		}
 
 		goto bo_import_skip;
-	}
-
-	fb->bo = gbm_bo_import(backend->gbm, GBM_BO_IMPORT_FD_MODIFIER,
-			       &import_mod, GBM_BO_USE_SCANOUT);
-	if (!fb->bo) {
-		if (try_view_on_plane_failure_reasons)
-			*try_view_on_plane_failure_reasons |=
-				FAILURE_REASONS_GBM_BO_IMPORT_FAILED;
-		goto err_free;
 	}
 
 bo_import_skip:
@@ -513,6 +517,35 @@ bo_import_skip:
 	}
 
 	fb->num_planes = attributes->n_planes;
+	if (dmabuf->gem_handles[0] == 0) {
+		for (i = 0; i < attributes->n_planes; i++) {
+			int ret;
+			ret = drmPrimeFDToHandle (fb->fd, attributes->fd[i], &gem_handle[i]);
+			if (ret) {
+				weston_log ("got gem_handle %x\n", gem_handle[i]);
+				goto err_free;
+			}
+			fb->handles[i] = dmabuf->gem_handles[i] = gem_handle[i];
+		}
+		linux_dmabuf_buffer_gem_handle_close_cb (dmabuf, drm_close_gem_handle);
+	} else {
+		for (i = 0; i < attributes->n_planes; i++)
+			fb->handles[i] = dmabuf->gem_handles[i];
+	}
+
+	if (fb->handles[0] != 0)
+		goto add_fb;
+
+	ARRAY_COPY(import_mod.fds, attributes->fd);
+	ARRAY_COPY(import_mod.strides, attributes->stride);
+	ARRAY_COPY(import_mod.offsets, attributes->offset);
+
+	fb->bo = gbm_bo_import(backend->gbm, GBM_BO_IMPORT_FD_MODIFIER,
+			       &import_mod, GBM_BO_USE_SCANOUT);
+	if (!fb->bo)
+		goto err_free;
+
+	fb->num_planes = attributes->n_planes;
 	for (i = 0; fb->bo && i < attributes->n_planes; i++) {
 		union gbm_bo_handle handle;
 
@@ -525,6 +558,7 @@ bo_import_skip:
 		fb->handles[i] = handle.u32;
 	}
 
+add_fb:
 	if (drm_fb_addfb(device, fb) != 0) {
 		if (try_view_on_plane_failure_reasons)
 			*try_view_on_plane_failure_reasons |=

libweston/backend-drm/drm-internal.h
@@ -86,6 +86,8 @@
 #define MAX_DMABUF_PLANES 4
 #endif
 
+#define ALIGNTO(a, b) ((a + (b-1)) & (~(b-1)))
+
 /**
  * A small wrapper to print information into the 'drm-backend' debug scope.
  *

libweston/linux-dmabuf.c
@@ -158,6 +158,9 @@ destroy_linux_dmabuf_wl_buffer(struct wl_resource *resource)
 	weston_assert_ptr_eq(buffer->compositor, buffer->buffer_resource, resource);
 	weston_assert_ptr_null(buffer->compositor, buffer->params_resource);
 
+	if (buffer->gem_handle_close_func)
+		buffer->gem_handle_close_func(buffer);
+
 	if (buffer->user_data_destroy_func)
 		buffer->user_data_destroy_func(buffer);
 
@@ -977,6 +980,21 @@ linux_dmabuf_buffer_get(struct weston_compositor *compositor,
 	weston_assert_ptr_eq(compositor, buffer->buffer_resource, resource);
 
 	return buffer;
+}
+
+/** Set drmbackend-private data
+ *
+ * set the drm gem handle close callback in the linux_dmabuf_buffer
+ *
+ * \param buffer The linux_dmabuf_buffer object to set for.
+ * \param func Destructor function to be called to close gem handle
+ *             when the linux_dmabuf_buffer gets destroyed.
+ */
+WL_EXPORT void
+linux_dmabuf_buffer_gem_handle_close_cb(struct linux_dmabuf_buffer *buffer,
+				  dmabuf_gem_handle_close_func func)
+{
+	buffer->gem_handle_close_func = func;
 }
 
 /** Set renderer-private data

libweston/linux-dmabuf.h
@@ -33,6 +33,8 @@
 struct linux_dmabuf_buffer;
 typedef void (*dmabuf_user_data_destroy_func)(
 			struct linux_dmabuf_buffer *buffer);
+typedef void (*dmabuf_gem_handle_close_func)(
+			struct linux_dmabuf_buffer *buffer);
 
 struct dmabuf_attributes {
 	int32_t width;
@@ -72,6 +74,9 @@ struct linux_dmabuf_buffer {
 
 	/**< marked as scan-out capable, avoids any composition */
 	bool direct_display;
+
+	uint32_t gem_handles[MAX_DMABUF_PLANES];
+	dmabuf_gem_handle_close_func gem_handle_close_func;
 };
 
 enum weston_dmabuf_feedback_tranche_preference {
@@ -153,6 +158,10 @@ weston_direct_display_setup(struct weston_compositor *compositor);
 struct linux_dmabuf_buffer *
 linux_dmabuf_buffer_get(struct weston_compositor *compositor,
 			struct wl_resource *resource);
+
+void
+linux_dmabuf_buffer_gem_handle_close_cb(struct linux_dmabuf_buffer *buffer,
+				  dmabuf_gem_handle_close_func func);
 
 void
 linux_dmabuf_buffer_set_user_data(struct linux_dmabuf_buffer *buffer,

commit 60401491c0a924f95a65b99aafeaed8358b73af9

backend-drm: fix invalid container_of for internal renderbuffer dmabuf
drm_fb_get_from_dmabuf_attributes() uses container_of() to recover
a linux_dmabuf_buffer from the attributes pointer. This is only
valid when attributes is embedded in linux_dmabuf_buffer (client
dmabufs). For internal renderbuffers (Vulkan path), attributes is
a standalone heap allocation, so container_of produces a bogus
pointer and gem_handles access reads garbage, causing black screen.

Fixes: be368c1 ("compositor-drm: support get drm fb from dmabuf")

Upstream-Status: Inappropriate [i.MX-specific]
Siged-off-by: Ryan Zhang <ryan.zhang@nxp.com>

libweston/backend-drm/fb.c
@@ -457,7 +457,6 @@ drm_fb_get_from_dmabuf_attributes(struct dmabuf_attributes *attributes,
 	struct drm_fb *fb;
 	int i;
 	uint32_t gem_handle[MAX_DMABUF_PLANES] = {0};
-	struct linux_dmabuf_buffer *dmabuf = container_of(attributes, struct linux_dmabuf_buffer, attributes);
 	struct gbm_import_fd_modifier_data import_mod = {
 		.width = attributes->width,
 		.height = attributes->height,
@@ -548,20 +547,41 @@ bo_import_skip:
 	}
 
 	fb->num_planes = attributes->n_planes;
-	if (dmabuf->gem_handles[0] == 0) {
+
+	/* For internal renderbuffers, attributes is standalone heap memory,
+	 * not embedded in linux_dmabuf_buffer; use drmPrimeFDToHandle directly.
+	 * For client dmabufs, recover the parent via container_of and use
+	 * the cached gem_handles[]. */
+	if (is_internal) {
 		for (i = 0; i < attributes->n_planes; i++) {
 			int ret;
-			ret = drmPrimeFDToHandle (fb->fd, attributes->fd[i], &gem_handle[i]);
+			ret = drmPrimeFDToHandle(fb->fd, attributes->fd[i], &gem_handle[i]);
 			if (ret) {
-				weston_log ("got gem_handle %x\n", gem_handle[i]);
+				weston_log("drmPrimeFDToHandle failed for plane %d: %s\n",
+					   i, strerror(errno));
 				goto err_free;
 			}
-			fb->handles[i] = dmabuf->gem_handles[i] = gem_handle[i];
+			fb->handles[i] = gem_handle[i];
 		}
-		linux_dmabuf_buffer_gem_handle_close_cb (dmabuf, drm_close_gem_handle);
 	} else {
-		for (i = 0; i < attributes->n_planes; i++)
-			fb->handles[i] = dmabuf->gem_handles[i];
+		struct linux_dmabuf_buffer *dmabuf =
+			container_of(attributes, struct linux_dmabuf_buffer, attributes);
+
+		if (dmabuf->gem_handles[0] == 0) {
+			for (i = 0; i < attributes->n_planes; i++) {
+				int ret;
+				ret = drmPrimeFDToHandle (fb->fd, attributes->fd[i], &gem_handle[i]);
+				if (ret) {
+					weston_log ("got gem_handle %x\n", gem_handle[i]);
+					goto err_free;
+				}
+				fb->handles[i] = dmabuf->gem_handles[i] = gem_handle[i];
+			}
+			linux_dmabuf_buffer_gem_handle_close_cb (dmabuf, drm_close_gem_handle);
+		} else {
+			for (i = 0; i < attributes->n_planes; i++)
+				fb->handles[i] = dmabuf->gem_handles[i];
+		}
 	}
 
 	if (fb->handles[0] != 0)